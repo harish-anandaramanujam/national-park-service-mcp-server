8e89c1d427934fdbd04886b19d309ee9ee9e371c
Harish <harishneyveli96@gmail.com>
2025-07-31 16:13:02 -0700

    adding 8 more tools

--- server.py
+++ server.py
@@ -59,7 +59,7 @@ async def get_articles_tool(args: ParkModelArgs):
     return await src.api_utils.nps_get_park(args, client_url)
 
 @mcp.tool()
-async def get_activity_list_tool(args: GeneralModelArgs):
+async def get_all_activity_list_tool(args: GeneralModelArgs):
     """Tool that could Retrieve categories of activities 
     (astronomy, hiking, wildlife watching, etc.) and its ID
     possible in all national parks."""
@@ -67,6 +67,78 @@ async def get_activity_list_tool(args: GeneralModelArgs):
     client_url = nps_api_base_url + "/activities"
 
     return await src.api_utils.nps_get_general(args, client_url)
+
+@mcp.tool()
+async def get_park_specific_activity_list_tool(args: ParkModelArgs):
+    """Tool that could Retrieve national parks that are related to 
+    particular categories of activity (astronomy, hiking, wildlife watching, etc.)
+    in a specific park."""
+    
+    client_url = nps_api_base_url + "/activities/parks"
+
+    return await src.api_utils.nps_get_park(args, client_url)
+
+@mcp.tool()
+async def get_all_amenities_list_tool(args: GeneralModelArgs):
+    """Tool that could Retrieve the amenity types 
+    such as accessible restrooms, fire pit, picnic area, etc. 
+    available in all national parks."""
+    
+    client_url = nps_api_base_url + "/amenities"
+
+    return await src.api_utils.nps_get_general(args, client_url)
+
+@mcp.tool()
+async def get_amenities_parkplaces_tool(args: ParkModelArgs):
+    """Tool that could Retrieve "places" within national parks 
+    that have different amenities"""
+    
+    client_url = nps_api_base_url + "/amenities/parksplaces"
+
+    return await src.api_utils.nps_get_park(args, client_url)
+
+@mcp.tool()
+async def get_park_specific_visitorcenter_with_amenities_tool(args: ParkModelArgs):
+    """Tool that could Retrieve visitor centers within a specific national parks 
+    that have different amenities."""
+    
+    client_url = nps_api_base_url + "/amenities/parksvisitorcenters"
+
+    return await src.api_utils.nps_get_park(args, client_url)
+
+@mcp.tool()
+async def get_campgrounds_tool(args: ParkModelArgs):
+    """Tool that could Retrieve data about National Park Service 
+    campgrounds including addresses, contacts, description, 
+    hours of operation, etc."""
+    
+    client_url = nps_api_base_url + "/campgrounds"
+
+    return await src.api_utils.nps_get_park(args, client_url)
+
+@mcp.tool()
+async def get_events_tool(args: ParkModelArgs):
+    """Tool that retrieves events from parks including dates, 
+    descriptions, and times created by national parks and other entities"""
+    
+    client_url = nps_api_base_url + "/events"
+    return await src.api_utils.nps_get_park(args, client_url)
+
+@mcp.tool()
+async def get_feespasses_tool(args: ParkModelArgs):
+    """Tool that retrieves information about fees and passes 
+    created by national parks and other NPS entities"""
+    
+    client_url = nps_api_base_url + "/feespasses"
+    return await src.api_utils.nps_get_park(args, client_url)
+
+@mcp.tool()
+async def get_lesson_plans_tool(args: ParkModelArgs):
+    """Tool that retrieve lesson plans created by national 
+    parks and other NPS entities."""
+    
+    client_url = nps_api_base_url + "/lessonplans"
+    return await src.api_utils.nps_get_park(args, client_url)
 
 
 if __name__ == "__main__":
