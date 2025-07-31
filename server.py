da0598cbb7b1dceef149cd6133bcbff854159c7d
Harish <harishneyveli96@gmail.com>
2025-07-30 18:45:26 -0700

    Update server.py

--- server.py
+++ server.py
@@ -16,33 +16,33 @@ if not os.getenv("NPS_API_KEY"):
     load_dotenv()
 NPS_API_KEY = os.getenv("NPS_API_KEY")
 
-class ParkModelArgs(BaseModel):
-    park_code: Optional[str] = Field(description="Park code from NPS website")
-    state_code: Optional[str] = Field(description="US state where the park is addresed")
-    search_term: Optional[str] = Field(description="Term to search in the API output")
+# class ParkModelArgs(BaseModel):
+#     park_code: Optional[str] = Field(description="Park code from NPS website")
+#     state_code: Optional[str] = Field(description="US state where the park is addresed")
+#     search_term: Optional[str] = Field(description="Term to search in the API output")
 
     
 
-@mcp.tool()
-def get_park(args: ParkModelArgs):
-    """API request to NPS website to get retrieve data about national parks (addresses, contacts, description, hours of operation, etc.)"""
-    params = {
-        "parkCode": args.park_code,
-        "stateCode": args.state_code,
-        "q": args.search_term,
-        "api_key": os.getenv("NPS_API_KEY")
-    }
-    headers = {
-        "accept": "application/json"
-    }
-    client_url = nps_api_base_url + "/parks"
+# @mcp.tool()
+# def get_park(args: ParkModelArgs):
+#     """API request to NPS website to get retrieve data about national parks (addresses, contacts, description, hours of operation, etc.)"""
+#     params = {
+#         "parkCode": args.park_code,
+#         "stateCode": args.state_code,
+#         "q": args.search_term,
+#         "api_key": os.getenv("NPS_API_KEY")
+#     }
+#     headers = {
+#         "accept": "application/json"
+#     }
+#     client_url = nps_api_base_url + "/parks"
     
-    try:
-        response = requests.get(client_url, params=params, headers=headers)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as err:
-        return {"error": str(err)}
+#     try:
+#         response = requests.get(client_url, params=params, headers=headers)
+#         response.raise_for_status()
+#         return response.json()
+#     except requests.exceptions.RequestException as err:
+#         return {"error": str(err)}
 
 
 @mcp.tool()
