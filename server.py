8810a67e261d7b7b42ca24d08b3e7b7ef87bbf57
Harish <harishneyveli96@gmail.com>
2025-07-31 13:16:19 -0700

    add 2 more tools

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ import requests
 from pydantic import BaseModel, Field
 from mcp.server.fastmcp import FastMCP
 from typing import Optional
-from src.app import reverse_text
 import src.api_utils
 from src.models import ParkModelArgs
 
@@ -17,34 +16,6 @@ if not os.getenv("NPS_API_KEY"):
     load_dotenv()
 NPS_API_KEY = os.getenv("NPS_API_KEY")
 
-
-# class ParkModelArgs(BaseModel):
-#     park_code: Optional[str] = Field(description="Park code from NPS website")
-#     state_code: Optional[str] = Field(description="US state where the park is addresed")
-#     search_term: Optional[str] = Field(description="Term to search in the API output")
-
-    
-
-# @mcp.tool()
-# async def get_park(args: ParkModelArgs):
-#     """API request to NPS website to get retrieve data about national parks (addresses, contacts, description, hours of operation, etc.)"""
-#     params = {
-#         "parkCode": args.park_code,
-#         "stateCode": args.state_code,
-#         "q": args.search_term,
-#         "api_key": os.getenv("NPS_API_KEY")
-#     }
-#     headers = {
-#         "accept": "application/json"
-#     }
-#     client_url = nps_api_base_url + "/parks"
-    
-#     try:
-#         response = requests.get(client_url, params=params, headers=headers)
-#         response.raise_for_status()
-#         return response.json()
-#     except requests.exceptions.RequestException as err:
-#         return {"error": str(err)}
 
 @mcp.tool()
 async def get_park_tool(args: ParkModelArgs):
@@ -65,14 +36,28 @@ async def get_passport_stamp_locations_tool(args: ParkModelArgs):
     client_url = nps_api_base_url + "/passportstamplocations"
 
     return await src.api_utils.nps_get(args, client_url)
-    
+
 
 @mcp.tool()
-async def reverse_text_tool(text: str) -> str:
-    """
-    MCP server to reverse a text
-    """
-    return reverse_text(text)
+async def get_alerts_tool(args: ParkModelArgs):
+    """Tool that could Retrieve alerts 
+    (danger, closure, caution, and information) 
+    posted by parks."""
+    
+    client_url = nps_api_base_url + "/alerts"
+
+    return await src.api_utils.nps_get(args, client_url)
+
+@mcp.tool()
+async def get_articles_tool(args: ParkModelArgs):
+    """Tool that could Retrieve articles created by national parks 
+    and other NPS entities. 
+    (See "people" and "places" below for other specific article types.)"""
+    
+    client_url = nps_api_base_url + "/articles"
+
+    return await src.api_utils.nps_get(args, client_url)
+
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
--- src/app.py
+++
@@ -1,6 +0,0 @@
-# app.py
-def reverse_text(text: str) -> str:
-    """
-    reverse a text
-    """
-    return text[::-1]
